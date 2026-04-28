38e57d77850bf06c6f6f3e2f3d917da128f43ae8
Edson Vinícius Ferrari <vinybag@gmail.com>
2026-04-28 08:59:24 -0300

    Dropdown de troca no nome do funcionário

--- app.py
+++ app.py
@@ -108,56 +108,27 @@ def gerar_escala():
 @login_required
 def ver_escala():
     escala = Escala.query.filter_by(ativa=True).order_by(Escala.horario, Escala.funcionario_id, Escala.dia_semana).all()
-    return render_template('escala.html', escala=escala)
-
-if __name__ == '__main__':
-    app.run(debug=True)
+    todos_funcionarios = Funcionario.query.filter_by(ativo=True).all()
+    return render_template('escala.html', escala=escala, todos_funcionarios=todos_funcionarios)
 
 @app.route('/trocar-escala/<int:func1_id>/<int:func2_id>/<int:dia>')
 @login_required
 def trocar_escala(func1_id, func2_id, dia):
-    # Buscar as escalas ativas dos dois funcionários no dia específico
-    escala_func1 = Escala.query.filter_by(
-        funcionario_id=func1_id,
-        dia_semana=dia,
-        ativa=True
-    ).first()
+    # Buscar todas as escalas ativas dos dois funcionários
+    escalas_func1 = Escala.query.filter_by(funcionario_id=func1_id, ativa=True).all()
+    escalas_func2 = Escala.query.filter_by(funcionario_id=func2_id, ativa=True).all()
     
-    escala_func2 = Escala.query.filter_by(
-        funcionario_id=func2_id,
-        dia_semana=dia,
-        ativa=True
-    ).first()
+    # Trocar funcionario_id em todas as escalas
+    for escala in escalas_func1:
+        escala.funcionario_id = func2_id
     
-    if escala_func1 and escala_func2:
-        # Trocar os horários
-        horario_temp = escala_func1.horario
-        escala_func1.horario = escala_func2.horario
-        escala_func2.horario = horario_temp
-        db.session.commit()
-        flash('Troca realizada com sucesso!', 'success')
-    elif escala_func1 and not escala_func2:
-        # Func1 trabalha, Func2 folga -> Func1 folga, Func2 trabalha
-        funcionario = Funcionario.query.get(func2_id)
-        # Descobrir qual horário o func2 deveria ter
-        # Buscar outros dias do func2 para saber o horário dele
-        outra_escala = Escala.query.filter_by(
-            funcionario_id=func2_id,
-            ativa=True
-        ).first()
-        
-        if outra_escala:
-            # Criar escala para func2 no lugar do func1
-            nova_escala = Escala(
-                funcionario_id=func2_id,
-                dia_semana=dia,
-                horario=escala_func1.horario,
-                data=escala_func1.data,
-                ativa=True
-            )
-            db.session.add(nova_escala)
-            db.session.delete(escala_func1)
-            db.session.commit()
-            flash('Troca com folga realizada!', 'success')
+    for escala in escalas_func2:
+        escala.funcionario_id = func1_id
     
-    return redirect(url_for('ver_escala'))    
+    db.session.commit()
+    
+    func1 = Funcionario.query.get(func1_id)
+    func2 = Funcionario.query.get(func2_id)
+    flash(f'Troca realizada: {func1.nome} ⇄ {func2.nome}', 'success')
+    
+    return redirect(url_for('ver_escala'))
